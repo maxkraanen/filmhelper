54ef0652d4a3904d01e8f42d78cbed0f6949d66b
Max Kraanen <max@maxkraanen.com>
2017-09-06 13:51:39 +0200

    configure better errors for development only

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require_relative "../api.rb"
-require "better_errors"
+
 
 configure :development do
+  require "better_errors"
   use BetterErrors::Middleware
   BetterErrors.application_root = __dir__
 end
